9c192e49d4a35a0dc613ab896e8e0f0c991ce93d
John Bakhmat <johnbakhmat@gmail.com>
2023-11-11 15:25:14 +0300

    Initial Commit

---
+++ lib/qsmplive.ex
@@ -0,0 +1,18 @@
+defmodule Qsmplive do
+  @moduledoc """
+  Documentation for `Qsmplive`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Qsmplive.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/qsmplive_test.exs
@@ -0,0 +1,8 @@
+defmodule QsmpliveTest do
+  use ExUnit.Case
+  doctest Qsmplive
+
+  test "greets the world" do
+    assert Qsmplive.hello() == :world
+  end
+end
