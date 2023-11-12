f2cfc6ef297f24b4e49968f1689bf4f098df0728
John Bakhmat <johnbakhmat@gmail.com>
2023-11-12 15:07:28 +0300

    combined online and offline peopo into same json  + sorting

--- lib/qsmplive/web/app/controller.ex
+++ lib/qsmplive/web/app/controller.ex
@@ -12,16 +12,32 @@ defmodule Qsmplive.Web.Controller do
 
   plug(:dispatch)
 
+  defp merge_members(all, online) do
+    lowercase_online = Enum.map(online, fn x -> String.downcase(x) end)
+
+    Enum.map(all, fn member ->
+      channel = member["twitch"]
+
+      # The channel can be string or list of strings 
+      is_online =
+        if is_list(channel) do
+          Enum.any?(channel, fn x -> String.downcase(x) in lowercase_online end)
+        else
+          String.downcase(channel) in lowercase_online
+        end
+
+      Map.put(member, "is_live", is_online) |> dbg()
+    end)
+  end
+
   get "/" do
     {status, resp} =
       with {:ok, all_members} <- Api.get_all_members(),
            {:ok, online_members} <- Api.get_online_members(all_members) do
+        members = merge_members(all_members, online_members) 
+          |> Enum.sort_by(&{&1["is_live"], &1["team"], &1["name"]}, :desc)
 
-
-        {200, %{
-          "all_members" => all_members,
-          "online_members" => online_members
-        }}
+        {200, members}
       else
         {:error, error} ->
           Logger.error("Error: #{inspect(error)}")
