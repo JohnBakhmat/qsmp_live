4198fb4a751875c0e4043e9460758e4b4e107c03
John Bakhmat <johnbakhmat@gmail.com>
2023-11-12 18:06:31 +0300

    tailwind ?

---
+++ compile_tailwind.sh
@@ -0,0 +1,1 @@
+npx tailwindcss -i ./tailwind.in.css -o ./priv/styles.css --watch
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./lib/qsmplive/web/templates/**/*.html.eex"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
